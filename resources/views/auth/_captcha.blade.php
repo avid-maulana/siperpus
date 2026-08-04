e61129f3012cedf4f4e4accd520cbb2d42ba0c7f
sempakmumbul <avidmuhammad128@gmail.com>
2026-08-04 11:12:03 +0700

    feat:home dibagi jadi 2, admin dan user

--- resources/views/auth/_captcha.blade.php
+++ resources/views/auth/_captcha.blade.php
@@ -1,17 +1,17 @@
 @php
-    $angka1 = rand(1, 20);
-    $angka2 = rand(1, 30);
+$angka1 = rand(1, 20);
+$angka2 = rand(1, 30);
 
-    $ops = ['+', '-'];
-    $op = $ops[array_rand($ops)];
+$ops = ['+', '-'];
+$op = $ops[array_rand($ops)];
 
-    $hasil = $op === '+'
-        ? $angka1 + $angka2
-        : $angka1 - $angka2;
+$hasil = $op === '+'
+? $angka1 + $angka2
+: $angka1 - $angka2;
 
-    session(['captcha_hasil' => $hasil]);
+session(['captcha_hasil' => $hasil]);
 
-    $expr = "$angka1 $op $angka2";
+$expr = "$angka1 $op $angka2";
 @endphp
 
 <div class="captcha-box">
@@ -23,16 +23,14 @@
                 id="captchaCanvas"
                 width="220"
                 height="72"
-                class="captcha-canvas"
-            ></canvas>
+                class="captcha-canvas"></canvas>
         </div>
 
         <button
             type="button"
             id="captchaRefresh"
             class="captcha-refresh"
-            aria-label="Refresh Captcha"
-        >
+            aria-label="Refresh Captcha">
             <span class="icon">↻</span>
         </button>
 
@@ -48,13 +46,11 @@
             inputmode="text"
             autocomplete="off"
             placeholder=" "
-            class="w-full captcha-input"
-        >
+            class="w-full captcha-input">
 
         <label
             for="captcha"
-            class="captcha-label"
-        >
+            class="captcha-label">
             Masukkan Jawaban
         </label>
 
@@ -62,8 +58,7 @@
             id="captchaExpected"
             type="hidden"
             value="{{ session('captcha_hasil') }}"
-            data-expr="{{ $expr }}"
-        >
+            data-expr="{{ $expr }}">
 
     </div>
 
